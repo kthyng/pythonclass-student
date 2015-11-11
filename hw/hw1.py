8c2b4ba599838be6ea5207e3d27e9a54964d192e
Kristen M. Thyng <kthyng@gmail.com>
2015-11-11 15:15:27 -0600

    my hw soln

--- hw/hw1.py
+++ hw/hw1.py
@@ -9,6 +9,13 @@ def make_array():
     '''Make and return an array of ones with size (1000,10)
     '''
 
-def array_mean():
+    a = np.ones((1000, 10))
+
+    return list(a)
+
+
+def array_mean(a):
     '''Return the mean of the input array
     '''
+
+    return a.mean()
